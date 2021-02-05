2bd54424a0104a70cf2c2bc86c66cb4e04fe2c62
Anna Nawrocka <anna.nawrocka1988@gmail.com>
2021-02-05 13:10:32 +0000

    Add tests and solutions for all user stories

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,10 +1,28 @@
 class Dice
+  
+  def initialize
+    @previous_results = []
+  end 
+
   def roll(number_of_dice = 1 ) 
     results = []
     number_of_dice.times do      
       results << rand(1..6)      
     end
+    @previous_results << results
     results 
+  end 
+
+  def results
+    @previous_results
+  end
+
+  def score
+    sum = 0
+    @previous_results.each do |result|
+      sum += result.sum      
+    end
+    sum
   end 
 
 end 
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -19,9 +19,24 @@ describe Dice do
 
   it "given 2 dice roll should give me 2 numbers between 1 and 6" do
     subject = Dice.new
-    expect(subject.roll(2)[0]).to be_between(1, 6) 
-    expect(subject.roll(2)[1]).to be_between(1, 6) 
+    expect(subject.roll(2)).to all( be_between(1,6) )
+    # expect(subject.roll(2)[0]).to be_between(1, 6) 
+    # expect(subject.roll(2)[1]).to be_between(1, 6) 
   end
 
+  it "records previous dice roll" do
+    subject = Dice.new
+    expected_roll = subject.roll
+    expect(subject.results).to eq [expected_roll]
+  end
+
+  it "sums up all previous results as current score" do
+    subject = Dice.new
+    subject.roll #[1-6]
+    subject.roll(2) #[1-6, 1-6]
+    expect(subject.score).to be_between(3, 18)
+  end
 
 end
+
+#expect(subject.roll(10)).to all( be_between(1,6) )        
